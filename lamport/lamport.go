6779346fef29e5ca6a3a1c0b9001207e224d07a3
Joao Henrique <joao.henrique1299@hotmail.com>
2025-04-11 21:07:09 -0300

    feat: add initial structure to use lamport signature
    missing implement sign and veririe signature

--- lamport/lamport.go
+++ lamport/lamport.go
@@ -9,91 +9,92 @@ import (
 )
 
 type signarute struct {
-  PickedNumbers [][]byte
-  Publickey [][] byte
+	PickedNumbers [][]byte
+	Publickey     [][]byte
 }
 
 type LamportSignature struct {
-  hashAlgorithm hash.Hash
-  AlgorithmSize int
+	hashAlgorithm hash.Hash
+	algorithmSize int
 
-  PrivateKey [][] byte
+	privateKey [][]byte
 
-  Message string
-  MessageSignature signarute
+	Message          string
+	MessageSignature signarute
 
-  // use 4 to generate numbers of 32 bits
-  // use 8 to generate numbers of 64 bits
-  privateKeySize int
+	// use 4 to generate numbers of 32 bits
+	// use 8 to generate numbers of 64 bits
+	privateKeySize int
 }
 
 func (l *LamportSignature) genPrivateKey() {
-  for i := 0; i < l.AlgorithmSize * 2; i++ {
-    number := make([]byte, l.privateKeySize)
-    rand.Read(number)
-    l.PrivateKey = append(l.PrivateKey, number)
-  }
+	for i := 0; i < l.algorithmSize*2; i++ {
+		number := make([]byte, l.privateKeySize)
+		rand.Read(number) // nolint: gosec
+		l.privateKey = append(l.privateKey, number)
+	}
 }
 
-// func (l LamportSignature) GenPublicKey() {
-//   for v := range l.PrivateKey {
-//     l.hashAlgorithm.Write(v)
-//     l.MessageSignature.Publickey = append(l.MessageSignature.Publickey, hash)
-//   }
-// }
-
-func (l LamportSignature) Signmessage(message string) {
-
+func (l *LamportSignature) genPublicKey() {
+	for _, v := range l.privateKey {
+		l.hashAlgorithm.Write(v)
+		hash := l.hashAlgorithm.Sum(nil)
+		l.MessageSignature.Publickey = append(l.MessageSignature.Publickey, hash)
+	}
 }
 
-func (l LamportSignature) Validatesignature(message string) {
+func (l *LamportSignature) SignMessage(message string) {
 
 }
 
-func (l* LamportSignature) selectHashAlgorithm (hashAlgorithmName string) {
-  switch hashAlgorithmName {
-  case "SHA256":
-      l.hashAlgorithm = sha256.New()
-      l.AlgorithmSize = 256
-  case "SHA512":
-      l.hashAlgorithm = sha512.New()
-      l.AlgorithmSize = 512
-  }
+func (l *LamportSignature) ValidateSignature(message string) {
 
+}
+
+func (l *LamportSignature) selectHashAlgorithm(hashAlgorithmName string) {
+	switch hashAlgorithmName {
+	case "SHA256":
+		l.hashAlgorithm = sha256.New()
+		l.algorithmSize = 256
+	case "SHA512":
+		l.hashAlgorithm = sha512.New()
+		l.algorithmSize = 512
+	}
 }
 
 func LamportBuilder(hashAlgorithmName string, privateKeySize int) LamportSignature {
-  lamport := LamportSignature{}
+	lamport := LamportSignature{}
 
-  if !(privateKeySize == 4 || privateKeySize == 8) {
-    panic("privatekeysize must be eq to 4 or 8 to use numbers of 32 or 64 bits")
-  }
-  lamport.privateKeySize = privateKeySize
+	if !(privateKeySize == 4 || privateKeySize == 8) {
+		panic("privatekeysize must be eq to 4 or 8 to use numbers of 32 or 64 bits")
+	}
+	lamport.privateKeySize = privateKeySize
 
-  (&lamport).selectHashAlgorithm(hashAlgorithmName)
-  (&lamport).genPrivateKey()
+	(&lamport).selectHashAlgorithm(hashAlgorithmName)
+	(&lamport).genPrivateKey()
+	(&lamport).genPublicKey()
 
-
-  return lamport
+	return lamport
 }
 
-func LamportBuilderInformingPrivateKey(hashAlgorithmName string,  privateKey [][]byte) LamportSignature {
-  lamport := LamportSignature{}
+func LamportBuilderInformingPrivateKey(hashAlgorithmName string, privateKey [][]byte) LamportSignature {
+	lamport := LamportSignature{}
 
-  (&lamport).selectHashAlgorithm(hashAlgorithmName)
-  if len(privateKey) != lamport.AlgorithmSize * 2 {
-    panic(fmt.Sprintf("invalid number of bytes given for privateKey, for %v you must a private key with %v numbers", hashAlgorithmName, lamport.AlgorithmSize * 2))
-  }
+	(&lamport).selectHashAlgorithm(hashAlgorithmName)
+	if len(privateKey) != lamport.algorithmSize*2 {
+		panic(fmt.Sprintf("invalid number of bytes given for privateKey, for %v you must a private key with %v numbers", hashAlgorithmName, lamport.algorithmSize*2))
+	}
 
-  lamport.PrivateKey = privateKey
+	lamport.privateKey = privateKey
 
-  lamport.privateKeySize = len(privateKey[0])
+	lamport.privateKeySize = len(privateKey[0])
 
-  privateKeySize := lamport.privateKeySize
-  if !(privateKeySize == 4 || privateKeySize == 8) {
-    panic("privatekeysize must be eq to 4 or 8 to use numbers with 32 or 64 bits")
-  }
+	privateKeySize := lamport.privateKeySize
+	if !(privateKeySize == 4 || privateKeySize == 8) {
+		panic("private key size must be eq to 4 or 8 to use numbers with 32 or 64 bits")
+	}
 
+	lamport.genPublicKey()
 
-  return lamport
+	return lamport
 }
